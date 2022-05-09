0a36075c8d3becb743950920e042d7883b7c6e5f
JeremyStubbs <jeremystubbs@jeremys-mbp.myfiosgateway.com>
2022-05-09 13:22:57 -0400

    got rid of localization attempt

--- webcam.py
+++ webcam.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-import ast
-import scipy   
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2     
@@ -10,37 +8,32 @@ from keras.models import Model
 import time
 
 
-new_model = tf.keras.models.load_model('saved_model_asl/my_model')
+model = tf.keras.models.load_model('saved_model_asl/my_model')
 
-out1 = new_model.layers[-4].output
+label_map = ["A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"]
 
-out2 = new_model.layers[-1].output
-
-
-model = tf.keras.Model(inputs=new_model.input, 
-    outputs=(out1, out2)) 
-
-all_amp_layer_weights = model.layers[-1].get_weights()[0]
-
-labels=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-threshold = 0.5
+threshold = 0.25
 
 cap = cv2.VideoCapture(0)
+last_time = time.time()
+
 
 while True:
-    # time.sleep(0.5)
     success,img = cap.read()
-    resized = cv2.resize(img, (224, 224))
-    reshaped = resized.reshape(1, 224, 224, 3)
-    
-    last_conv_output, predictions = model.predict(reshaped)
-    
-    final_arr = []
+    if time.time()-last_time > 0.5:
+        last_time = time.time()
+        resized = cv2.resize(img, (224, 224))
+        resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2RGB)
 
-    for val in predictions[0]:
-        if val > threshold:
-            index_of = np.where(predictions[0]==val)
-            print(labels[index_of[0][0]], index_of[0][0])
+        reshaped = resized.reshape(1, 224, 224, 3)
+
+        predictions = model.predict(reshaped, verbose=0)
+
+        max_val = np.amax(predictions)
+        if max_val > threshold:
+            print(label_map[np.argmax(predictions)])
+
         
     cv2.imshow("output",img)
    
@@ -50,7 +43,3 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-
-##The following code "localizes", but causes the program to be too slow. 
-# Need to train with bounding box. 
-# Don't need to change the model much - just change final output layer to -3 (gap layer) for the box.
